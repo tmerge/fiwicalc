9c765fddd05cf4d6a98532110678af41e3a6c93e
tmerge <tmerge@uos.de>
2021-03-05 14:00:14 +0100

    implement captial value calc

--- calculator.py
+++ calculator.py
@@ -2,12 +2,34 @@
 
 import argparse
 import numpy
+import math
+
+# function to calc end value
+def calcVT(a, i, t):
+    a = numpy.array(a)
+    vt = 0
+    q = float(1 + (i/100))
+    counter = 0
+    if len(a) <= 0:
+        print("series of payment is empty")
+        return 0
+    if t <= 0: 
+        print("period of time is to short")
+        return 0
+    for value in a:
+        vt += value * q**(t - counter)
+        counter += 1
+    return numpy.round_(vt, 4)
 
 
 def main(args):
-    print(args)
+    # check what value user is looking for
     if args.vt != None:
-        print("calc vt")
+        resultVT = calcVT(args.a, args.i, args.t)
+        print(f"Result for VT: {resultVT}")
+    elif args.v0 != None:
+        # TODO: implement calculation of present value
+        print("calc v0")
 
 
 
@@ -21,12 +43,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Fiwi key figures calucaltion")
     parser.add_argument("-vt", help="calculate end value", action="store_const", default=vt, const=not(vt))
     parser.add_argument("-v0", help="calcualte net present value", action="store_const", default=v0, const=not(v0))
-    parser.add_argument("-a", help="set series of payment")
-    parser.add_argument("-i", help="set interest rate")
-    parser.add_argument("-t", help="set period")
+    parser.add_argument("-a", help="set series of payment", nargs="+", type=float)
+    parser.add_argument("-i", help="set interest rate", type=float)
+    parser.add_argument("-t", help="set period", type=float)
     args = parser.parse_args()
-
-    print("1")
-
+    
     # call calculator with args
     main(args)
